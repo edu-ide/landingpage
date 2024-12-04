bec1049cf2a8b2d7a68c7b417ad4ee28b795e1e6
dudnjsckrgo <dudnjsckrgo@gmail.com>
2024-12-05 00:05:58 +0900

    title 다국어 처리

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,8 +1,42 @@
+import { Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
 import {notFound} from 'next/navigation';
 import {routing} from '@/i18n/routing';
  
+type Props = {
+  params: { locale: string }
+};
+
+export async function generateMetadata({ params: { locale } }: Props): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: 'metadata' });
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    keywords: t('keywords'),
+    openGraph: {
+      title: t('og.title'),
+      description: t('og.description'),
+      images: [
+        {
+          url: t('og.image'),
+          width: 1200,
+          height: 630,
+          alt: t('og.imageAlt'),
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: t('twitter.title'),
+      description: t('twitter.description'),
+      images: [t('twitter.image')],
+    },
+  };
+}
+
 export default async function LocaleLayout({
   children,
   params
